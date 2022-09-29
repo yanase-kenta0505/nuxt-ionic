f3e3a85c109404ad245df94745481d82eae8a235
yanasekenta <ynsknt0505@gmail.com>
2022-09-30 07:59:12 +0900

    ionicの反映を確認

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <!-- 必ず頭がion-pageで始まるようにする -->
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Home</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content>
+      <h1>Hello world</h1>
+    </ion-content>
+  </ion-page>
+</template>
