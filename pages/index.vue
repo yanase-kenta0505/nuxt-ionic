3e314ce8f36141e5cd3d43a45d7e3d92a1507d0b
yanasekenta <ynsknt0505@gmail.com>
2022-10-02 01:02:57 +0900

    ページ遷移を実装

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,27 @@
+<script setup>
+  import { arrowForwardCircle } from 'ionicons/icons';
+  import { useIonRouter } from '@ionic/vue';
+
+  const ionRouter = useIonRouter()
+  const moveToTest = () => ionRouter.push('/test')
+</script>
+
+
 <template>
   <!-- 必ず頭がion-pageで始まるようにする -->
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Home</ion-title>
+        <ion-title>hello</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content>
       <h1>Hello world</h1>
+      <ion-fab vertical="bottom" horizontal="end" slot="fixed">
+        <ion-fab-button @click="moveToTest">
+          <ion-icon :icon="arrowForwardCircle" />
+        </ion-fab-button>
+      </ion-fab>
     </ion-content>
   </ion-page>
 </template>
---
+++ pages/test.vue
@@ -0,0 +1,24 @@
+<script setup>
+  import { arrowBackCircle } from 'ionicons/icons';
+  import { useIonRouter } from '@ionic/vue';
+  const ionRouter = useIonRouter()
+  const moveToHome = () => ionRouter.back()
+</script>
+
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>test</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content>
+      <h1>welcome test page</h1>
+      <ion-fab vertical="bottom" horizontal="start" slot="fixed">
+        <ion-fab-button @click="moveToHome">
+          <ion-icon :icon="arrowBackCircle" />
+        </ion-fab-button>
+      </ion-fab>
+    </ion-content>
+  </ion-page>
+</template>
