8908309766274c5364cce19827f8d8c204f75e6b
yanasekenta <ynsknt0505@gmail.com>
2022-10-02 11:00:49 +0900

    ionicのCSSユーティリティが効くように修正

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,5 +8,20 @@ export default defineNuxtConfig({
     define: {
       'window.global': {}
     }
-  }
+  },
+  css: [
+    /* Core CSS required for Ionic components to work properly */
+    '@ionic/vue/css/core.css',
+    /* Basic CSS for apps built with Ionic */
+    '@ionic/vue/css/normalize.css', 
+    '@ionic/vue/css/structure.css', 
+    '@ionic/vue/css/typography.css',
+    /* Optional CSS utils that can be commented out */
+    '@ionic/vue/css/padding.css', 
+    '@ionic/vue/css/float-elements.css', 
+    '@ionic/vue/css/text-alignment.css', 
+    '@ionic/vue/css/text-transformation.css', 
+    '@ionic/vue/css/flex-utils.css', 
+    '@ionic/vue/css/display.css',
+  ]
 })
--- pages/test.vue
+++ pages/test.vue
@@ -1,24 +1,27 @@
 <script setup>
-  import { arrowBackCircle } from 'ionicons/icons';
   import { useIonRouter } from '@ionic/vue';
-  const ionRouter = useIonRouter()
-  const moveToHome = () => ionRouter.back()
 </script>
 
 <template>
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>test</ion-title>
+        <ion-buttons slot="start">
+          <ion-back-button />
+        </ion-buttons>
+        <ion-title>TODOを作成</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content>
-      <h1>welcome test page</h1>
-      <ion-fab vertical="bottom" horizontal="start" slot="fixed">
-        <ion-fab-button @click="moveToHome">
-          <ion-icon :icon="arrowBackCircle" />
-        </ion-fab-button>
-      </ion-fab>
+      <ion-item>
+        <ion-label>タスク名</ion-label>
+        <ion-input />
+      </ion-item>
+      <ion-item>
+        <ion-label>説明</ion-label>
+        <ion-input />
+      </ion-item>
+      <ion-button expand="block" class="ion-margin-top">追加</ion-button>
     </ion-content>
   </ion-page>
 </template>
