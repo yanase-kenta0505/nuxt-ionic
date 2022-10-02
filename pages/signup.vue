4afb30d02917e472c982f95a2f77087c4beba787
yanasekenta <ynsknt0505@gmail.com>
2022-10-02 18:20:14 +0900

    新規会員登録ページを作成

--- pages/signup.vue
+++ pages/signup.vue
@@ -1,7 +1,31 @@
 <script setup>
+  import { Auth } from 'aws-amplify';
+
   const username = ref('')
-  const pass = ref('')
+  const password = ref('')
   const email = ref('')
+
+  const signup = async () => {
+    try {
+      const { user } = await Auth.signUp({
+        "username": username.value,
+        "password": password.value,
+        attributes: {
+          "email": email.value
+        },
+        autoSignIn: {
+          enabled: true
+        }
+      }) 
+      console.log(user)
+    } catch (error) {
+      console.log('error')
+    }
+
+    username.value = ''
+    password.value = ''
+    email.value = ''
+  }
 </script>
 
 <template>
@@ -18,13 +42,13 @@
       </ion-item>
       <ion-item>
         <ion-label>パスワード</ion-label>
-        <ion-input v-model="pass" />
+        <ion-input v-model="password" />
       </ion-item>
       <ion-item>
         <ion-label>メールアドレス</ion-label>
         <ion-input v-model="email" />
       </ion-item>
-      <ion-button expand="block" class="ion-margin-top" @click="addTask">登録</ion-button>
+      <ion-button expand="block" class="ion-margin-top" @click="signup">登録</ion-button>
     </ion-content>
   </ion-page>
 </template>
